b307f600f461d211c49b232c0d5304466004f01c
artur3087 <152414564+artur3087@users.noreply.github.com>
2024-03-18 12:34:39 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+const audio = document.getElementById("mp3");  
+  audio.volume = 0.1;  // Property .volume describes volume from 0.0 (muted), 0.5 (half volume) to 1.0 (100% volume). It can be added to any variable used for audio...  
+
+// Other example:
+
+// const mp3 = document.getElementById("mp3");
+//     mp3.volume = 0.1; 
